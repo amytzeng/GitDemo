bee1ca0f1dd5738c177e8f6c7b05faa01454acb8
amytzeng <148669475+amytzeng@users.noreply.github.com>
2023-12-07 20:42:19 +0800

    Update helloWorld.cpp

--- helloWorld.cpp
+++ helloWorld.cpp
@@ -4,6 +4,9 @@ using namespace std;
 // Todo: 寫一個印出"Hello World"的程式
 int main()
 {
-	cout << "Hello World!" << "\n";
+	for(int i - 0; i < 5; i++)
+	{
+		cout << "Hello World!" << "\n";
+	}
 	return 0;
 }
